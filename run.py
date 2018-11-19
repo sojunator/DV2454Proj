49e5894847e8ca7c815050852a847129015c4ce9
Carl Söyseth <carlsoyseth@gmail.com>
2018-11-19 22:23:42 +0100

    Basic network generation and forwardpropagation implemented

--- run.py
+++ run.py
@@ -2,23 +2,47 @@ import gym
 import numpy as np
 
 
-env = gym.make("CartPole-v1")
-observation = env.reset()
-
-# Since observation can be of any dim, we first flatten the array
-inputLayer = observation.flatten()
-actionSpace = env.action_space
-
-dimensions = {}
-
-dimensions["inputLayer"] = inputLayer
-dimensions["actionSpace"] = actionSpace
-dimensions["n"] = 2
-dimensions["m"] = inputLayer + 2
-
-
 def sigmoid(x):
     return 1 / (1 + np.exp(x))
 
 def createNetwork(dimensions):
-    weights = np.random.rand(dimensions["m"], dimensions["n"])
+    network = []
+    hiddenLayer = 2 * np.random.random((dimensions["inputLayer"], dimensions["inputLayer"] + 2)) - 1
+
+    network.append(hiddenLayer)
+
+    for i in range(dimensions["nrOfHiddenLayers"]):
+        hiddenLayer = 2 * np.random.random((dimensions["inputLayer"] + 2, dimensions["inputLayer"] + 2)) - 1
+        network.append(hiddenLayer)
+
+    hiddenLayer = 2 * np.random.random((dimensions["inputLayer"] + 2, 2)) - 1
+
+    network.append(hiddenLayer)
+
+    return network
+
+def forwardPropagation(inputLayer, network):
+    Z = inputLayer
+    for layer in network:
+        Z = np.dot(Z, layer)
+    return Z
+
+
+if __name__ == '__main__':
+
+    env = gym.make("CartPole-v1")
+    observation = env.reset()
+
+    # Since observation can be of any dim, we first flatten the array
+    inputLayer = observation.flatten()
+    actionSpace = env.action_space
+
+    dimensions = {}
+
+    dimensions["inputLayer"] = len(inputLayer)
+    dimensions["nrOfHiddenLayers"] = 1
+
+
+    network = createNetwork(dimensions)
+    action = forwardPropagation(inputLayer, network)
+    print(action)
