c144a8db316166bc9514fc7509c36537e6daa4e0
Carl Söyseth <carlsoyseth@gmail.com>
2018-11-21 18:49:05 +0100

    Re worked structure of how networks and score is tracked

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import gym
 import numpy as np
+import itertools
 
 def sigmoid(x):
     return 1 / (1 + np.exp(x))
@@ -15,7 +16,12 @@ def genetics(mother, father, split):
         child.append(np.concatenate((np.hsplit(mLayer, split)[0],
                                      np.hsplit(fLayer, split)[0]), 1))
 
+
     children.append(child)
+    # We have to clear child, otherwise the structure return yields a incorrect
+    # network
+    child.clear()
+
     # Do the same procedure again, but take the other halfs
     for mLayer, fLayer in zip(mother, father):
         child.append(np.concatenate((np.hsplit(mLayer, split)[1],
@@ -27,14 +33,18 @@ def genetics(mother, father, split):
 
 def createNetwork(dimensions):
     network = []
+    # Fist layer is a corner case, as we need to have correct dims for the input
     hiddenLayer = 2 * np.random.random((dimensions["inputLayer"], dimensions["inputLayer"] + 2)) - 1
 
     network.append(hiddenLayer)
 
     for i in range(dimensions["nrOfHiddenLayers"]):
+        # Usage of "InputLayer" key is not accurate, as it's actually the number of
+        # Neurons
         hiddenLayer = 2 * np.random.random((dimensions["inputLayer"] + 2, dimensions["inputLayer"] + 2)) - 1
         network.append(hiddenLayer)
 
+    # Output layer is also a corner case, as it needs to result in a 1xNumberOfActions Matrix
     hiddenLayer = 2 * np.random.random((dimensions["inputLayer"] + 2, dimensions["outputLayer"])) - 1
 
     network.append(hiddenLayer)
@@ -44,7 +54,11 @@ def createNetwork(dimensions):
 def forwardPropagation(inputLayer, network):
     Z = inputLayer
     for layer in network:
-        Z = sigmoid(np.dot(Z, layer))
+        try:
+            Z = sigmoid(np.dot(Z, layer))
+        except ValueError:
+            print("Z {}".format(Z.shape))
+            print("Layer {}\n\n\n\n".format(layer.shape))
 
     # Convert outputlayer to a regular list
     return Z.tolist()
@@ -84,6 +98,8 @@ if __name__ == '__main__':
     env = gym.make("CartPole-v1")
     observation = env.reset()
     nrOfNetworks = 1000
+    generations = 1000
+    keepPerGen = 100
 
     # Since observation can be of any dim, we first flatten the array
     inputLayer = observation.flatten()
@@ -95,41 +111,37 @@ if __name__ == '__main__':
     dimensions["nrOfHiddenLayers"] = 1
     dimensions["outputLayer"] = actionSpace.n
 
-    # Scores will contain all scores for the respektive network
+    # Contains a tuple with score and the network, score starts at zero
     networks = []
-    scores = []
 
     # Create our networks and store them in networks list
     for i in range(nrOfNetworks):
-        networks.append(createNetwork(dimensions))
+        networks.append([0, createNetwork(dimensions)])
 
-    children = genetics(networks[0], networks[1], 2)
-    
-    for i in range(1000):
+    children = genetics(networks[0][1], networks[1][1], 2)
+    networks.append([0, children[0]])
+    networks.append([0, children[1]])
+
+
+    for i in range(generations):
         for network in networks:
-            scores.append(testNetwork(network, 201, env))
+            network[0] = testNetwork(network[1], 201, env)
 
-        # Find networks that are good for mutation and breeding
-        # If a network does not pass criteria, it get removed
-        # criteria is currently being larger than average in score
-        averageScore = sum(scores) / len(scores)
-        print(averageScore)
-        removedNetWorkCounter = 0
-        for score in scores:
-            if score < averageScore:
-                index = scores.index(score)
 
-                networks.pop(index)
-                scores.pop(index)
-
-                removedNetWorkCounter += 1
+        # Sort networks by performance
+        # Keep top 100
+        networks.sort(key=lambda x : x[0])
+        networks = networks[-keepPerGen:]
 
         # Apply genetics and breeding
+        tempPairs = list(itertools.combinations(networks, 2))
+
+        # iterate over 2 networks pairs, and create two new children
+        # Merge these into our network list
 
         #Generate new networks to refill the pool
-        for i in range(removedNetWorkCounter):
-            networks.append(createNetwork(dimensions))
+        for i in range(nrOfNetworks - len(networks)):
+            networks.append([0,createNetwork(dimensions)])
 
-        # reset scores
-        del scores[:]
+
     env.close()
